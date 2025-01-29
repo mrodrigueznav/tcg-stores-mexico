ef963fe6d7b7f2e164fca89e8d71c94852ddb6c6
mrodrigueznav <mrodrigueznav@gmail.com>
2025-01-28 22:30:58 -0600

    fix base in build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tcg-stores-mexico/',
+  base: '',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
